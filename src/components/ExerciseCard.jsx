016c8f9f01534262cc7e394b5de075b24d55e797
wgarv100 <78126184+wgarv100@users.noreply.github.com>
2023-01-02 10:24:55 -0500

    Add exercise card and add grid to card search results.

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,4 +1,13 @@
-import { Container, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardAction,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 
@@ -13,9 +22,31 @@ const ExerciseCard = ({ exercise }) => {
   return (
     <Container maxWidth="xl">
       <Box>
-        <img src={exercise.gifUrl} alt={exercise.name} />
-        <Typography variant="h5">{exercise.name}</Typography>
-        <Typography variant="p">{exercise.target}</Typography>
+        <Card>
+          <CardMedia
+            component="img"
+            alt={exercise.name}
+            image={exercise.gifUrl}
+          />
+          <CardContent sx={{ pb: 2, height: "75px" }}>
+            <Typography variant="h5" sx={{ pb: 1 }}>
+              {exercise.name.toUpperCase()}
+            </Typography>
+            <Typography variant="body2">
+              {exercise.target.toUpperCase()}
+            </Typography>
+          </CardContent>
+          <CardActionArea>
+            <Button
+              variant="contained"
+              color="error"
+              size="medium"
+              sx={{ m: 2 }}
+            >
+              Add
+            </Button>
+          </CardActionArea>
+        </Card>
       </Box>
     </Container>
   );
--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography, Container } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import ExerciseCard from "./ExerciseCard";
@@ -6,12 +6,18 @@ import ExerciseCard from "./ExerciseCard";
 const Exercises = ({ exercises }) => {
   return (
     <Box>
-      <Typography variant="h4" color="black">
+      <Typography variant="h4" color="black" sx={{ p: 3 }}>
         Results
       </Typography>
-      {exercises?.map((exercise, id) => (
-        <ExerciseCard key={id} exercise={exercise} />
-      ))}
+      <Container maxWidth="xl">
+        <Grid container spacing={1}>
+          {exercises?.map((exercise, id) => (
+            <Grid item xs={12} md={4}>
+              <ExerciseCard key={id} exercise={exercise} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </Box>
   );
 };
