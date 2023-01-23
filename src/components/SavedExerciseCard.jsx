c56494e0716f74009be2248ee75c6ba806538330
Wesley Garvin <garvinwes@gmail.com>
2023-01-23 11:07:33 -0500

    Add delete saved exercise functionality. - When delete button is clicked page is refreshed with updated exercises. - Same saved exercise is displayed three times in saved exercises page.

--- src/components/SavedExerciseCard.jsx
+++ src/components/SavedExerciseCard.jsx
@@ -5,48 +5,59 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import { Box, Container } from "@mui/system";
-import React from "react";
+import { Container } from "@mui/system";
+import React, { useEffect, useState } from "react";
 
-const SavedExerciseCard = ({ exercise }) => {
+const SavedExerciseCard = () => {
+  const [exercises, setExercises] = useState([]);
+  const [toggleDelete, setToggleDelete] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/savedExercises")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setExercises(data);
+      });
+  }, [toggleDelete]);
+
   const deleteSavedExercise = async (exercise) => {
-    fetch(`http://localhost:3000/savedExercises/${exercise.id}`, {
+    await fetch(`http://localhost:3001/savedexercises/${exercise.id}`, {
       method: "DELETE",
     });
+    setToggleDelete(!toggleDelete);
   };
 
   return (
-    <>
-      <Container maxWidth="xl">
-        <Box>
-          <Card>
-            <CardMedia
-              component="img"
-              alt={exercise.name}
-              image={exercise.gifUrl}
-            />
-            <CardContent sx={{ pb: 2, height: "75px" }}>
-              <Typography variant="h5" sx={{ pb: 1 }}>
-                {exercise.name.toUpperCase()}
-              </Typography>
-              <Typography variant="body2">
-                {exercise.target.toUpperCase()}
-              </Typography>
-            </CardContent>
-            <Button
-              variant="outlined"
-              color="error"
-              size="medium"
-              sx={{ mt: 2, mb: 2, ml: 1 }}
-              // passes the specific exercise object data
-              onClick={() => deleteSavedExercise(exercise)}
-            >
-              Delete
-            </Button>
-          </Card>
-        </Box>
-      </Container>
-    </>
+    <Container maxWidth="xl">
+      {exercises.map((exercise) => (
+        <Card key={exercise.id}>
+          <CardMedia
+            component="img"
+            alt={exercise.name}
+            image={exercise.gifUrl}
+          />
+          <CardContent sx={{ pb: 2, height: "75px" }}>
+            <Typography variant="h5" sx={{ pb: 1 }}>
+              {exercise.name.toUpperCase()}
+            </Typography>
+            <Typography variant="body2">
+              {exercise.target.toUpperCase()}
+            </Typography>
+          </CardContent>
+          <Button
+            variant="outlined"
+            color="error"
+            size="medium"
+            sx={{ mt: 2, mb: 2, ml: 1 }}
+            onClick={() => deleteSavedExercise(exercise)}
+          >
+            Delete
+          </Button>
+        </Card>
+      ))}
+    </Container>
   );
 };
 
