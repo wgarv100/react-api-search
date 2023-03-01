8665a12bece738192619f124f8f5a4a0da28cf29
Wesley Garvin <garvinwes@gmail.com>
2023-03-01 12:54:22 -0500

    Fix duplicate saved exercise cards bug.

--- src/components/SavedExerciseCard.jsx
+++ src/components/SavedExerciseCard.jsx
@@ -6,21 +6,10 @@ import {
   Typography,
 } from "@mui/material";
 import { Container } from "@mui/system";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const SavedExerciseCard = () => {
-  const [exercises, setExercises] = useState([]);
+const SavedExerciseCard = ({ exercise }) => {
   const [toggleDelete, setToggleDelete] = useState(false);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/savedExercises")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setExercises(data);
-      });
-  }, [toggleDelete]);
 
   const deleteSavedExercise = async (exercise) => {
     await fetch(`http://localhost:3001/savedexercises/${exercise.id}`, {
@@ -31,32 +20,30 @@ const SavedExerciseCard = () => {
 
   return (
     <Container maxWidth="xl">
-      {exercises.map((exercise) => (
-        <Card key={exercise.id}>
-          <CardMedia
-            component="img"
-            alt={exercise.name}
-            image={exercise.gifUrl}
-          />
-          <CardContent sx={{ pb: 2, height: "75px" }}>
-            <Typography variant="h5" sx={{ pb: 1 }}>
-              {exercise.name.toUpperCase()}
-            </Typography>
-            <Typography variant="body2">
-              {exercise.target.toUpperCase()}
-            </Typography>
-          </CardContent>
-          <Button
-            variant="outlined"
-            color="error"
-            size="medium"
-            sx={{ mt: 2, mb: 2, ml: 1 }}
-            onClick={() => deleteSavedExercise(exercise)}
-          >
-            Delete
-          </Button>
-        </Card>
-      ))}
+      <Card>
+        <CardMedia
+          component="img"
+          alt={exercise.name}
+          image={exercise.gifUrl}
+        />
+        <CardContent sx={{ pb: 2, height: "75px" }}>
+          <Typography variant="h5" sx={{ pb: 1 }}>
+            {exercise.name.toUpperCase()}
+          </Typography>
+          <Typography variant="body2">
+            {exercise.target.toUpperCase()}
+          </Typography>
+        </CardContent>
+        <Button
+          variant="outlined"
+          color="error"
+          size="medium"
+          sx={{ m: 2 }}
+          onClick={() => deleteSavedExercise(exercise)}
+        >
+          Delete
+        </Button>
+      </Card>
     </Container>
   );
 };
